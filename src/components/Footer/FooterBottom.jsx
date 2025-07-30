7996ded45344343a929ee353050e10fbbcca70ea
Dilouar Hossain <dilouarbd@gmail.com>
2025-07-30 17:12:17 +0600

    commit

--- src/components/Footer/FooterBottom.jsx
+++ src/components/Footer/FooterBottom.jsx
@@ -3,7 +3,7 @@ const FooterBottom = () => {
     <div className="card px-3 py-2 flex-row align-items-center justify-content-between footer-bottom">
       <p> &copy; 2025 Dhaka School & College. All Rights Reserved. </p>
       <p>
-        Develop By:{" "}
+        Develop By{" "}
         <a href="https://creativesoftware.com.bd/" target="_blank">
           Creative Software
         </a>
